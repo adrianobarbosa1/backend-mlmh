b57708453c0bcee0eee85bf36d5f1cbf8f4017b2
adrianobarbosa1 <adriano.barbosa100@gmail.com>
2022-06-28 22:50:26 -0300

    att

--- src/validations/register.validation.js
+++ src/validations/register.validation.js
@@ -1,50 +1,57 @@
-const Joi = require('joi')
+const { validator } = require('cpf-cnpj-validator');
+const Joi = require('joi').extend(validator);
 
 const createRegister = {
   body: Joi.object().keys({
     nome: Joi.string().required(),
     email: Joi.string().email().required(),
-    cpf: Joi.string()
-      .length(14)
-      .required()
-      .error(() => {
-        return { message: "CPF Inválido." };
-      }),
+    cpf: Joi.document().cpf(),
     rg: Joi.string()
       .required()
       .error(() => {
-        return { message: "RG não pode ficar vazio" };
+        return { message: 'RG não pode ficar vazio' };
       }),
-    orgao_emissor: Joi.string().required(),
     uf_rg: Joi.string().required(),
     dt_nascimento: Joi.date().required(),
-    fone: Joi.string().required(),
+    fone_celular: Joi.string().required(),
+    fone_fixo: Joi.string().optional(),
     sexo: Joi.string().required(),
+    portador_pcd: Joi.string().required(),
     estado_civil: Joi.string().required(),
     nacionalidade: Joi.string().required(),
     cep: Joi.string().required(),
-    uf: Joi.string().required(),
-    municipio: Joi.string().required(),
-    bairro: Joi.string().optional().allow(""),
-    quadra: Joi.string().optional().allow(""),
-    lote: Joi.string().optional().allow(""),
     logradouro: Joi.string().required(),
-    complemento: Joi.string().optional().allow(""),
-    tempo_reside: Joi.string().required(),
+    quadra: Joi.string().optional().allow(''),
+    lote: Joi.string().optional().allow(''),
+    complemento: Joi.string().optional().allow(''),
+    bairro: Joi.string().optional().allow(''),
+    municipio: Joi.string().required(),
+    uf: Joi.string().required(),
+    reside_ano: Joi.string().required(),
+    reside_mes: Joi.string().required(),
     renda_bruta: Joi.string().required(),
     cadunico: Joi.string().required(),
-    tempo_cadunico: Joi.string().optional(),
     numero_cadunico: Joi.string().optional(),
+    possui_imovel: Joi.string().required(),
+    contemplado_habitacional: Joi.string().required(),
+    comprador_imovel: Joi.string().required(),
+    arrimo_familia: Joi.string().required(),
     vitima_violencia: Joi.string().required(),
-    pcd: Joi.string().required(),
     grupo_familiar: Joi.string().required(),
-    gf_nome: Joi.string().optional(),
-    gf_cpf: Joi.string().optional(),
-    gf_dt_nascimento: Joi.string().optional(),
-    gf_grau_parentesco: Joi.string().optional(),
-    gf_quantidade: Joi.string().optional(),
-    gf_cpf_certidao: Joi.string().optional(),
-    gf_certidao: Joi.string().optional(),
+    integrantes: Joi.array()
+      .items(
+        Joi.object({
+          integrante: Joi.string().optional(),
+          gf_nome: Joi.string().optional(),
+          gf_dt_nascimento: Joi.string().optional(),
+          gf_cpf: Joi.document().cpf().optional(),
+          gf_rg_certidao: Joi.string().optional(),
+          gf_pcd: Joi.string().optional(),
+          gf_parentesco: Joi.string().optional(),
+        })
+      )
+      .optional()
+      .allow(''),
   }),
 };
 
@@ -63,11 +70,11 @@ const update = {
     nome: Joi.string().required(),
     cargo: Joi.string().required(),
     email: Joi.string().email().required(),
-    telefone: Joi.string().allow(""),
+    telefone: Joi.string().allow(''),
     cpf: Joi.string().min(14).required(),
     dt_nascimento: Joi.date().required(),
-    rg: Joi.string().optional().allow(""),
-    uf_rg: Joi.string().optional().allow(""),
+    rg: Joi.string().optional().allow(''),
+    uf_rg: Joi.string().optional().allow(''),
   }),
 };
 
@@ -80,5 +87,6 @@ const remove = {
 module.exports = {
   createRegister,
   getRegisters,
+  update,
   remove,
 };
